48b353a6bdc6b411e10f7eb4b6f69f1e5edb1410
elnanoesmipadre <rafiter28@gmail.com>
2025-07-04 18:18:15 +0200

    Update Roulette.java

--- src/Roulette/Roulette.java
+++ src/Roulette/Roulette.java
@@ -19,10 +19,11 @@ class Roulette{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         var credits = 1500;
+        System.out.println("Juguemos a la ruleta.");
         while(true){
-            int num = (int)(Math.random()*36);
-            int color = (int)(Math.random());
-            int dozen = (int)(Math.random()*2)+1;
+            int num = (int)(Math.random()*37);
+            int color = (int)(Math.random()*2);
+            int dozen = (int)(Math.random()*3)+1;
             int even_odd = num % 2;
             //Putting some results
 
@@ -32,7 +33,7 @@ class Roulette{
                 //Finishing the game if the user doesn't have any credits
             
             }else{
-                System.out.println("Juguemos a la ruleta.");
+                
                 System.out.println("Tienes " + credits + " monedas. ¿Cuanto dinero quieres apostar?");
                 int bet = input.nextInt();
                 input.nextLine();
@@ -51,35 +52,90 @@ class Roulette{
                 //choosing the mode
                 switch(election){
                     case "números", "numeros", "a numeros", "a números":
-                        System.out.println("A qué número apuestas?");
-                        int num_elegido = input.nextInt();
-                        input.nextLine();
-                        if(num_elegido == num && num_elegido <= 36 && num_elegido >= 0){
-                            System.out.println("¡Has ganado!");
-                            credits += bet * 35;
-                            resume_screen(input);
-                        }else if(num_elegido != num && num_elegido <= 36 && num_elegido >= 0){
-                            System.out.println("¡Has perdido! Había tocado el nº " + num);
-                            resume_screen(input);
-                        }else{
-                            System.out.println("Elige un número entre 0 y 36");
+                        boolean validNum = false;
+                        while(!validNum){
+                            System.out.println("A qué número apuestas?");
+                            int num_elegido = input.nextInt();
+                            input.nextLine();
+                            if(num_elegido == num && num_elegido <= 36 && num_elegido >= 0){
+                                System.out.println("¡Has ganado!");
+                                credits += bet * 35;
+                                resume_screen(input);
+                                validNum = true;
+                            }else if(num_elegido != num && num_elegido <= 36 && num_elegido >= 0){
+                                System.out.println("¡Has perdido! Había tocado el nº " + num);
+                                resume_screen(input);
+                                validNum = true;
+                            }else{
+                                System.out.println("Elige un número entre 0 y 36");
+                            }
                         }
+                        break;
                     case "color", "a color":
-                        System.out.println("¿Rojo o Negro?");
-                        String col_elegido = input.nextLine();
-                        if((col_elegido.equalsIgnoreCase("rojo") && color == 0) || (col_elegido.equalsIgnoreCase("negro") && color == 1)){
-                            System.out.println("¡Has acertado!");
+                        boolean colorValido = false;
+                        while (!colorValido) {
+                            System.out.println("¿Rojo o Negro?");
+                            String col_elegido = input.nextLine().trim().toLowerCase();
+                            if ((col_elegido.equals("rojo") && color == 0) || (col_elegido.equals("negro") && color == 1)) {
+                                System.out.println("¡Has acertado!");
+                                credits += bet * 2;
+                                resume_screen(input);
+                                colorValido = true;
+                            } else if ((col_elegido.equals("rojo") && color == 1) || (col_elegido.equals("negro") && color == 0)) {
+                                if (col_elegido.equals("rojo")) {
+                                    System.out.println("¡Has fallado! Había tocado negro");
+                                } else {
+                                    System.out.println("Lo siento... Había tocado rojo");
+                                }
+                                resume_screen(input);
+                                colorValido = true;
+                            } else {
+                                System.out.println("Por favor, ingrese rojo o negro");
+                                // El bucle repite la pregunta
+                            }
+                        }
+                        break;
+                    case "docenas", "a docenas":
+                        boolean validDozen = false;
+                        while(!validDozen){
+                            System.out.println("¿Juegas a la 1ª, a la 2ª o a la 3ª docena?");
+                            int dozen_chosen = input.nextInt();
+                            input.nextLine();
+                            if(dozen_chosen == dozen){
+                                System.out.println("¡Has ganado");
+                                credits += bet * 5;
+                                resume_screen(input);
+                                validDozen = true;
+                            }else if((dozen_chosen != dozen) && (dozen_chosen <= 3) && (dozen_chosen >= 1)){
+                                System.out.println("¡Has fallado! Había tocado la " + dozen + "ª docena");
+                                resume_screen(input);
+                                validDozen = true;
+                            }else{
+                                System.out.println("Ingrese 1, 2 o 3");
+                            }
+                        }
+                        break;
+                    case "pares", "a pares":
+                        if(even_odd == 0){
+                            System.out.println("¡Has ganado!");
                             credits += bet * 2;
                             resume_screen(input);
-                        }else if((col_elegido.equalsIgnoreCase("rojo") && color == 1) || (col_elegido.equalsIgnoreCase("negro") && color == 0)){
-                            System.out.println("¡Has fallado! Había tocado " + color);
                         }else{
-                            System.out.println("Por favor, ingrese rojo o negro");
+                            System.out.println("¡Has perdido! El número era " + num + ", que es impar.");
+                            resume_screen(input);
                         }
-                    case "docenas", "a docenas":
-                        System.out.println("¿Juegas a la 1ª, a la 2ª o a la 3ª docena?");
-                        int dozen_chosen = input.nextInt();
-                        input.nextLine();
+                    case "impares", "a impares":
+                        if(even_odd == 1){
+                            System.out.println("¡Has ganado!");
+                            credits += bet * 2;
+                            resume_screen(input);
+                        }else{
+                            System.out.println("¡Has perdido! El número era " + num + ", que es par");
+                            resume_screen(input);
+                        }
+                    default:
+                        System.out.println("Por favor, elige una de las opciones disponibles.");
+                        credits += bet;
                 }
             }
         }
